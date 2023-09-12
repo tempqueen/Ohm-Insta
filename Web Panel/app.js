0922017c5637c104568ec0b3963a42e8464dfa3d
tempqueen <144730396+tempqueen@users.noreply.github.com>
2023-09-12 14:36:25 +0800

    app.js

--- Web Panel/app.js
+++ Web Panel/app.js
@@ -2,13 +2,13 @@
 
 
     var firebaseConfig = {
-    apiKey: "API KEY",
-    authDomain: "AUTH DOMAIN",
-    databaseURL: "DATABSE URL",
-    projectId: "PROJECT ID",
-    storageBucket: "BUCKET URL",
-    messagingSenderId: "SENDER ID",
-    appId: "APP ID"
+    apiKey: "AIzaSyB7zZMztpRHrk9MCC3a6E5TPXVItANDQxg",
+    authDomain: "ohm-insta.firebaseapp.com",
+    databaseURL: "https://ohm-insta-default-rtdb.firebaseio.com/",
+    projectId: "ohm-insta",
+    storageBucket: "ohm-insta.appspot.com",
+    messagingSenderId: "1019885529226",
+    appId: "1:1019885529226:android:7a4712d96d5bc52fdfa573"
   }; 
  
 ////Do Not Edit Below This
